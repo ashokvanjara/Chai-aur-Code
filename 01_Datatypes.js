86b5205bcaac2a4899c600a1cdbb6887bcc7394c
ashokvanjara <ashok.vanjara@aimsinfosoft.com>
2023-10-02 12:28:53 +0530

    basic of data types with codewith chai

---
+++ 01_Datatypes.js
@@ -0,0 +1,12 @@
+let Accountnumber = 9978193564;
+let ifsc = "SBI000";
+const email = "ashokvanjara@2124@gail.com";
+const password = "ashok@2124";
+const address = "Dachka meghraj";
+const city = "modasa";
+const collge = "govenment polytechnic Dahod";
+const deprtment = "computer engineering";
+// const cannot be changed
+Accountnumber = 1234;
+ifsc = "ashok@1234"
+console.table([Accountnumber, ifsc, email, password, address, city, collge, deprtment]);
