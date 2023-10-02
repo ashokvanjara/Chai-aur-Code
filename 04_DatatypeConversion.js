9e061b7181a32d70a4b6c2301e210d4144874ae0
ashokvanjara <ashok.vanjara@aimsinfosoft.com>
2023-10-02 12:39:41 +0530

    this is the types of conversion

---
+++ 04_DatatypeConversion.js
@@ -0,0 +1,7 @@
+let score  = 1233;
+const name = "ashok vanjara";
+let double = 1.25
+let int  = 124;
+let result = false;
+console.log(typeof false);
+// console.table([score,name,double,int,result]);
