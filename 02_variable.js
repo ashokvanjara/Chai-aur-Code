d14b6a65bf6ed490acacb34ef9f87f11c3b5abf3
ashokvanjara <ashok.vanjara@aimsinfosoft.com>
2023-10-02 12:30:58 +0530

    start the new tutoral codewithchai

---
+++ 02_variable.js
@@ -0,0 +1,8 @@
+// creating a new variable
+const a = 12;
+const b = 12;
+let c = true;
+let d = 3.11455
+
+
+console.table([a,b,c,d]);
---
+++ 03_Datatype&Ecma.js
@@ -0,0 +1,3 @@
+// alert("hllo ashok");  we are using node js not browsser
+console.log(3 + 3);
+console.log( typeof"ashok vanjara");    
