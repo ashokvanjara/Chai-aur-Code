fed36b03fd95cc48c8ac73ae263270d1cfbd9a78
ashokvanjara <ashok.vanjara@aimsinfosoft.com>
2023-10-08 10:43:09 +0530

    create types of conversion

--- 04_DatatypeConversion.js
+++ 04_DatatypeConversion.js
@@ -1,7 +1,13 @@
 let score  = 1233;
 const name = "ashok vanjara";
 let double = 1.25
-
-let result = false;
-console.log(typeof false);
+let isslogin = "0";
+let booleanIsloggdin = Boolean(isslogin);
+console.log(booleanIsloggdin);
 // console.table([score,name,double,int,result]);
+
+// convert  number to string
+let number = 23;
+let stringnumber = String(number);
+console.log(stringnumber);
+console.log(typeof stringnumber);
---
+++ 05_conversionOpertaion.js
@@ -0,0 +1,37 @@
+// // let str1 = "vanjara";
+// // let str2 = " ashok"
+
+// // console.log(str1+str2);
+
+// // console.log("1"+2);
+// // console.log(1+"2");
+// // console.log("1"+2+2);
+// // console.log(1+2+"2");
+
+
+
+// // const fun = function ashok() {
+// //     console.log("ashok");
+// // }
+// // console.log(typeof fun);
+
+// let score = "ashok";
+// // console.log(typeof score);
+// // console.log(score);
+
+// let valuenumber = Number(score)
+// console.log(valuenumber);
+// console.log(typeof valuenumber);
+// console.log( valuenumber);
+
+
+// let islogddin = true;
+// let booleanloggdin = Boolean(islogddin);
+
+// console.log(booleanloggdin);
+
+
+let someNumber = 33;
+let stringcon =String(someNumber);
+console.log(stringcon);
+console.log(typeof stringcon);
